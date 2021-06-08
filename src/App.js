e68c9e6273a2d45b1a6f22eedeb388ed273a7aa2
priyanshu-lanjewar <priyanshu.lanjewar@yahoo.com>
2021-06-08 19:52:02 +0530

    App js Updaated

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
      <Header id="aa"></Header>
      <Stacks></Stacks>
      <Programing></Programing>
-     <Box mt="20px"p="5px"
+     <Box mt="25px"p="5px"
       bg={useColorModeValue('gray.50', 'gray.900')}
       color={useColorModeValue('gray.700', 'gray.200')} >
      
